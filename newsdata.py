399110137ef9b023a91f4cbe01e847504176e339
marinm <mestrovic.ma@gmail.com>
2017-10-01 11:34:17 -0400

    refactor/newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -1,15 +1,35 @@
-#! python3
-
+#!/usr/bin/env python3
 import psycopg2
 
+
+def connect(database_name="news"):
+    ''' 
+    Establish connection with datebase and return db and cursor,
+    takes news database as default parametar
+    '''
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Can't connect to " + database_name + "database")
+
+def printMessage(message):
+    '''
+    Prints appropriate message
+    '''
+    print('')
+    print(message)
+    print('')
+    return
 
 def three_articles_of_all_time():
     '''
         fetch and print the most popular three articles of all time
         based on number of the views
     '''
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+    db = connect()[0]
+    c = connect()[1]
     c.execute('''select articles.title, count(*) as num
                     from log, articles
                     where log.status = '200 OK' 
@@ -21,9 +41,7 @@ def three_articles_of_all_time():
     ''')
     top_articles = c.fetchall()
     db.close()
-    print('')
-    print('What are the most popular three articles of all time?')
-    print('')
+    printMessage('What are the most popular three articles of all time?')
     for article in top_articles:
         print('"' + str(article[0]) + '"' + ' - ' + str(article[1]))
     return
@@ -34,8 +52,8 @@ def all_time_authors():
         fetch and print the most popular authors of all time
         based on the sum of the views for all the articles each author has written
     '''
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+    db = connect()[0]
+    c = connect()[1]
     c.execute('''
         select authors.name, sum(subq.num) as summer
         from authors, articles, (
@@ -53,8 +71,7 @@ def all_time_authors():
     ''')
     top_authors = c.fetchall()
     db.close()
-    print('Who are the most popular article authors of all time?')
-    print('')
+    printMessage('Who are the most popular article authors of all time?')
     for author in top_authors:
         print(str(author[0]) + ' - ' + str(author[1]))
     return
@@ -65,9 +82,9 @@ def req_err_stats():
         Fetch error statistics for each date and log the specific day
         when more then 1% of request failed
     '''
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute('''select form_date, 100.0 * failed_reqs / reqs as perc
+    db = connect()[0]
+    c = connect()[1]
+    c.execute('''select form_date, round(100.0 * failed_reqs / reqs, 2) as perc
         from
             (select  time::timestamp::date as form_date, 
             sum(case when log.status != '' then 1 else 0 end) as reqs, 
@@ -81,11 +98,10 @@ def req_err_stats():
 
     days = c.fetchall()
     db.close()
-    print('On which days did more than 1% of requests lead to errors?')
-    print('')
+    printMessage('On which days did more than 1% of requests lead to errors?')
     for day in days:
         print(day[0].strftime("%B %d, %Y") + ' - ' +
-              str(round(day[1], 1)) + '% errors')
+              str(day[1]) + '% errors')
     return
 
 

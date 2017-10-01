2b65c6e0996ac365e4e321e74de528ee88b3ff3c
marinm <mestrovic.ma@gmail.com>
2017-10-01 16:57:33 -0400

    fix/all_time)authors query and refactor

--- newsdata.py
+++ newsdata.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
+
+''' module prints out data for newsdata db'''
+
 import psycopg2
 
 
-def connect(database_name="new"):
-    ''' 
-    Establish connection with datebase and return db and cursor,
-    takes news database as default parametar
-    '''
+def get_query_results(query):
+    ''' DB helper that handles interaction with the database'''
     try:
-        db = psycopg2.connect("dbname={}".format(database_name))
-        cursor = db.cursor()
-        return db, cursor
+        db = psycopg2.connect(database="news")
+        c = db.cursor()
+        c.execute(query)
+        result = c.fetchall()
+        db.close()
+        return result
     except psycopg2.Error as e:
-        print("Can't connect to " + database_name + " database")
         print(e)
-
-
-def printMessage(message):
-    '''
-    Prints appropriate message
-    '''
-    print('')
-    print(message)
-    print('')
-    return
+        exit(1)
 
 
 def three_articles_of_all_time():
@@ -31,22 +24,19 @@ def three_articles_of_all_time():
         fetch and print the most popular three articles of all time
         based on number of the views
     '''
-    db = connect()[0]
-    c = connect()[1]
-    c.execute('''select articles.title, count(*) as num
+    query = '''select articles.title, count(*) as num
                     from log, articles
-                    where log.status = '200 OK' 
+                    where log.status = '200 OK'
                     and log.path != '/'
                     and articles.slug = substring(log.path, 10)
                     group by articles.title
                     order by num desc
                     limit 3;
-    ''')
-    top_articles = c.fetchall()
-    db.close()
-    printMessage('What are the most popular three articles of all time?')
-    for article in top_articles:
-        print('"' + str(article[0]) + '"' + ' - ' + str(article[1]))
+    '''
+    top_articles = get_query_results(query)
+    print('\nWhat are the most popular three articles of all time?\n')
+    for title, views in top_articles:
+        print('"{}" - {} views'.format(title, views))
     return
 
 
@@ -56,27 +46,24 @@ def all_time_authors():
         based on the sum of the views for all the articles
         each author has written
     '''
-    db = connect()[0]
-    c = connect()[1]
-    c.execute('''select authors.name, sum(subq.num) as summer
-        from authors, articles, (
-                    select articles.title, count(*) as num
-                    from log, articles
-                    where log.status = '200 OK' 
-                    and log.path != '/'
-                    and articles.slug = substring(log.path, 10)
-                    group by articles.title
-                    order by num desc
-                    ) as subq
-        where authors.id = articles.author
+    query = '''select authors.name, sum(subq.num) as summer
+        from authors,
+            (select articles.title, articles.author, count(*) as num
+            from log, articles
+            where log.status = '200 OK'
+            and log.path != '/'
+            and articles.slug = substring(log.path, 10)
+            group by articles.title, articles.author
+            order by num desc
+            ) as subq
+        where authors.id =subq.author
         group by authors.name
         order by summer desc;
-    ''')
-    top_authors = c.fetchall()
-    db.close()
-    printMessage('Who are the most popular article authors of all time?')
-    for author in top_authors:
-        print(str(author[0]) + ' - ' + str(author[1]))
+    '''
+    top_authors = get_query_results(query)
+    print('\nWho are the most popular article authors of all time?\n')
+    for author, views in top_authors:
+        print('{} - {} views'.format(author, views))
     return
 
 
@@ -85,35 +72,24 @@ def req_err_stats():
         fetch error statistics for each date and log the specific day
         when more then 1% of request failed
     '''
-    db = connect()[0]
-    c = connect()[1]
-    c.execute('''select form_date, round(100.0 * failed_reqs / reqs, 2)
+    query = '''select form_date, round(100.0 * failed_reqs / reqs, 2)
      as perc from
-        (select  time::timestamp::date as form_date, 
-        sum(case when log.status != '' then 1 else 0 end) as reqs, 
-        sum(case when log.status != '200 OK' then 1 else 0 end) 
-        as failed_reqs from log 
+        (select  time::timestamp::date as form_date,
+        count(*) as reqs,
+        sum(case when log.status != '200 OK' then 1 else 0 end)
+        as failed_reqs from log
         group by form_date
         ) as stats_by_date
         where ( 100.0 * failed_reqs / reqs ) > 1;
-    ''')
-
-    days = c.fetchall()
-    db.close()
-    printMessage('On which days did more than 1% of requests lead to errors?')
+    '''
+    days = get_query_results(query)
+    print('\nOn which days did more than 1% of requests lead to errors?\n')
     for day in days:
-        print(day[0].strftime("%B %d, %Y") + ' - ' +
-              str(day[1]) + '% errors')
+        print(day[0].strftime("%B %d, %Y") +
+              ' - ' + str(day[1]) + '% errors\n')
     return
 
 
 three_articles_of_all_time()
-print('')
-print('')
-
 all_time_authors()
-print('')
-print('')
-
 req_err_stats()
-print('')
